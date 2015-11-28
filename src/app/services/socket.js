05f030658f5f29e13c9c903b4851aaf2ec1cf271
Wlad Meixner <wlad@dhcp-10-176-3-5.dynamic.eduroam.mwn.de>
2015-11-28 10:21:15 +0100

    loaded socket io into project also generated factory for io exposure

---
+++ src/app/services/socket.js
@@ -0,0 +1,24 @@
+// This factory exposes the socket io client as a angular factory
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+    });
+    }
+  };
+});
