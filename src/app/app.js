d782cec7e209daa78da31690bd5f32826aeae0cd
mog8017 <mog8017@thi.de>
2015-11-28 10:47:39 +0100

    login view added &assets missing

--- src/app/app.js
+++ src/app/app.js
@@ -5,20 +5,39 @@ angular.module('app', [
     'ui.bootstrap'
 ]);
 
-angular.module('app').config(function($stateProvider, $urlRouterProvider) {
+angular.module('app')
+    .config(function($stateProvider, $urlRouterProvider) {
     // For any unmatched url, redirect to the root.
     $urlRouterProvider.otherwise("/");
 
-    // Now set up the states
-    $stateProvider
-        .state('mainview', {
-            url: "/",
-            templateUrl : 'mainview/view.html',
-            controller  : 'mainviewCtrl'
+    // Now set up the routes
+        $routeProvider
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'loginview/login.html'
+            })
+
+            .when('/', {
+                controller  : 'mainviewCtrl',
+                templateUrl: 'modules/home/views/home.html'
+            })
+
+            .otherwise({ redirectTo: '/login' });
+
+});
+
+angular.module('app').run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
         });
-});
-
-angular.module('app').run(function () {
-
-});
-    // Set to the root scope.
+    }]);
